947ea155dfb6403300a084abb9352ea79aa2a038
Raquel Leão <vc.raquel@gmail.com>
2021-08-30 13:24:33 -0300

    exercicio 2

---
+++ src/br/com/zup/Exercicio2.java
@@ -0,0 +1,12 @@
+package br.com.zup;
+
+public class Exercicio2 {
+    public static void main(String[] args) {
+        int contador = 100;
+
+        while (contador <=100 & contador>0){
+            System.out.println(contador);
+            contador -=1;
+        }
+    }
+}
