57c91f7c8a843bb152b3dc341e618db353a695f7
Raquel Leão <vc.raquel@gmail.com>
2021-08-30 14:26:22 -0300

    lista 5 exercicio 3

---
+++ src/br/com/zup/lista5_Exercicio3.java
@@ -0,0 +1,33 @@
+package br.com.zup;
+
+import java.util.Scanner;
+
+public class lista5_Exercicio3 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        int quantidadeNumeros = 0;
+        int contador = 0;
+        int numerosImpares = 0;
+        int numerosPares = 0;
+
+
+        System.out.println("Por favor digite a quantidade de números desejados: ");
+        quantidadeNumeros = leitor.nextInt();
+
+        while (contador < quantidadeNumeros) {
+            System.out.println("Digite um numero");
+            double valorLido = leitor.nextDouble();
+            if (valorLido % 2 != 0) {
+                numerosImpares += 1;
+            } else {
+                numerosPares += 1;
+            }
+            contador += 1;
+
+        }
+        System.out.println("A quantidade de números impares nos " + quantidadeNumeros + " números escolhidos é: " + numerosImpares);
+        System.out.println("A quantidade de números pares nos " + quantidadeNumeros + " números escolhidos é: " + numerosPares);
+
+    }
+}
